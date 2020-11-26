0e449e7fc461b512bce6d7bc99b3f0127c1acbb6
Vlad Efremov <vladefr97@yandex.ru>
2020-11-26 11:16:15 -0500

    Header added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,36 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HomeComponent } from './pages/home/home.component';
-import { PainterComponent } from './units/project/painter/painter.component';
-import { AddBtnComponent } from './units/base/add-btn/add-btn.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HomeComponent} from './pages/home/home.component';
+import {PainterComponent} from './units/project/painter/painter.component';
+import {AddBtnComponent} from './units/base/add-btn/add-btn.component';
+import {HeaderComponent} from './units/project/header/header.component';
 
+import {
+  MatIconModule,
+} from '@angular/material/icon';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+const MaterialComponents = [MatIconModule];
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     PainterComponent,
-    AddBtnComponent
+    AddBtnComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MatIconModule
+
   ],
+  exports: [MaterialComponents],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
